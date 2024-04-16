7f4512c3f76c61d020e6a3e764d082d488029f2d
conangamo <161421620+conangamo@users.noreply.github.com>
2024-04-16 14:55:57 +0700

    Add files via upload

---
+++ DatabaseManager.java
@@ -0,0 +1,16 @@
+package GK2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/your_database";
+    private static final String USERNAME = "your_username";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
+
